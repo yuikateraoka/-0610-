1fc1c13106eb7bdc805a3e03f9ef474664a4ee7e
KimJisnsu66 <jinsu6688@gmail.com>
2020-06-10 12:02:51 +0900

    Initial sharing of project

---
+++ Book_Rental.java
@@ -0,0 +1,31 @@
+
+/**
+ * Book_Rental 클래스
+ * 
+ * @author (2020.06.10) 
+ * @version (2014671038 김진수,2018315030 이가영, 2018315053)
+ */
+public class Book_Rental
+{
+
+    /**
+     * 렌탈DVD할인 요금 계산 메소드 
+     * 
+     * @param  age   나이를 입력하는 매개변수
+     * @return  money 요금을 return 한다.  
+     */
+    public int discount_book(int age,int money,boolean old_book)
+    {
+        
+        
+        if(){
+        
+        }
+        else if(){
+        
+        }
+        
+        
+        return money;
+    }
+}
